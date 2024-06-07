2e0f3395424b29f4ce1e1465d8f709f28ecbe323
CarlsnOP <Anderscarlsen94@gmail.com>
2024-06-07 21:19:53 +0300

    init

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@onready var score_sound = $ScoreSound
+
+var _plane_died: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SignalManager.on_plane_died.connect(on_plane_died)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= delta * GameManager.SCROLL_SPEED
+
+
+func on_plane_died() -> void:
+	_plane_died = true
+	set_process(false)
+
+
+func _on_screen_exited():
+	queue_free()
+
+
+func _on_laser_body_exited(body):
+	if _plane_died == false and body.is_in_group(GameManager.GROUP_PLAYER) == true:
+		ScoreManager.increment_score()
+		score_sound.play()
+
+
+func _on_pipe_body_entered(body):
+	if body.is_in_group(GameManager.GROUP_PLAYER) == true:
+		if body.has_method("die") == true:
+			body.die()
